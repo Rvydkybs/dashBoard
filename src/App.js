d34ff750648508f0d87781302c8e774d7f24c738
Rüveyda Kayabaşı <rvyd.kybs@outlook.com>
2022-10-05 22:32:16 +0300

    BarChart included

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import "./App.css";
 import { useState } from "react";
+import "@shopify/polaris-viz/build/esm/styles.css";
+import { BarChart, PolarisVizProvider } from "@shopify/polaris-viz";
 
-import { jsonData } from "./JsonData/json_data(1)";
 import "./styles.css";
 import access from "./components/Tables/access";
 import forbidden from "./components/Tables/forbidden";
@@ -11,13 +12,13 @@ function App() {
   const [slider, setSlider] = useState(0);
   const mouseEvent = () => {
     setSlider(slider + 1);
-    if (slider == 2) {
+    if (slider === 2) {
       setSlider(0);
     }
   };
   return (
     <div className="container" style={{ margin: "10px" }}>
-      {slider == 0 ? access() : slider == 1 ? forbidden() : error()}
+      {slider === 0 ? access() : slider === 1 ? forbidden() : error()}
       <button onClick={mouseEvent} title="Switch" className="btn">
         Switch
       </button>
@@ -26,59 +27,49 @@ function App() {
           height: "500px",
         }}
       >
-        <PolarisVizProvider
-          themes={{
-            Default: {
-              chartContainer: {
-                padding: "20px",
-              },
+        <BarChart
+          data={[
+            {
+              data: [
+                {
+                  key: "Monday",
+                  value: 3,
+                },
+                {
+                  key: "Tuesday",
+                  value: -7,
+                },
+              ],
+              name: "Access",
             },
-          }}
-        >
-          <BarChart
-            data={[
-              {
-                data: [
-                  {
-                    key: "Monday",
-                    value: 3,
-                  },
-                  {
-                    key: "Tuesday",
-                    value: -7,
-                  },
-                ],
-                name: "Breakfast",
-              },
-              {
-                data: [
-                  {
-                    key: "Monday",
-                    value: 4,
-                  },
-                  {
-                    key: "Tuesday",
-                    value: 0,
-                  },
-                ],
-                name: "Lunch",
-              },
-              {
-                data: [
-                  {
-                    key: "Monday",
-                    value: 7,
-                  },
-                  {
-                    key: "Tuesday",
-                    value: 0,
-                  },
-                ],
-                name: "Dinner",
-              },
-            ]}
-          />
-        </PolarisVizProvider>
+            {
+              data: [
+                {
+                  key: "Monday",
+                  value: 4,
+                },
+                {
+                  key: "Tuesday",
+                  value: 0,
+                },
+              ],
+              name: "Forbidden",
+            },
+            {
+              data: [
+                {
+                  key: "Monday",
+                  value: 7,
+                },
+                {
+                  key: "Tuesday",
+                  value: 0,
+                },
+              ],
+              name: "Error",
+            },
+          ]}
+        />
       </div>
     </div>
   );
